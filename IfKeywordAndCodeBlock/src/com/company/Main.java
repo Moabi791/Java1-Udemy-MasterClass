fa9aba978cc2dea032726095d0677a940143c0c9
Moabi <moabilitsoane@gmail.com>
2021-10-19 11:01:24 +0200

    16th commit

--- IfKeywordAndCodeBlock/src/com/company/Main.java
+++ IfKeywordAndCodeBlock/src/com/company/Main.java
@@ -4,52 +4,45 @@ public class Main {
 
     public static void main(String[] args) {
         boolean gameOver = true;
-        int score = 800;
+        int score = 5000;
         int levelCompleted = 5;
         int bonus = 100;
 
-//        if(score == 5000 && >1000){
-//            System.out.println("Your score was less than 5000 but greater than 1000");
-//        } else if (score < 1000) {
+//        if (score < 5000) {
+//            System.out.println("Your score was less than 5000 nut greater than 1000");
+//        }else if(score < 1000) {
 //            System.out.println("Your score was less than 1000");
 //        }else {
 //            System.out.println("Got here");
-//    }
-
-        if(gameOver == true ) {
+//        }
+        if (gameOver == true) {
             int finalScore = score + (levelCompleted * bonus);
-            finalScore += 1000;
-            System.out.println("Your final score was " + finalScore);
+            System.out.println("Your final score was " + finalScore );
         }
-
 
         //Print out a second score on the screen with the following
         //score set to 1000
         //levelCompleted set to 8
-        //bonus set to 200
-        //But make sure the first printout above still displays as well
+        //But make sure the first printout above still display as well
 
+//            boolean newGameOver = true;
+//            int newScore = 10000;
+//            int newLevelCompleted = 8;
+//            int newBonus = 200;
 
-//        boolean newGameOver = true;
-//        int newScore = 10000;
-//        int newLevelCompleted = 8;
-//        int newBonus = 200;
-
-//        if (newGameOver) {
+//        if(newGameOver) {
 //            int finalScore = newScore + (newLevelCompleted * newBonus);
 //            System.out.println("Your final score was " + finalScore);
-//        }
+//         }
         score = 10000;
         levelCompleted = 8;
         bonus = 200;
-        if(gameOver) {
+
+        if (gameOver) {
             int finalScore = score + (levelCompleted * bonus);
             System.out.println("Your final score was " + finalScore);
-
         }
 
 
-
-        }
     }
-
+}
